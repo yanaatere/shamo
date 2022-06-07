2872f2a8c677a6e84bff53951401497e04d851f4
yanaatere <yanaandika@gmail.com>
2022-06-07 17:53:42 +0700

    Create Splah Page And Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/pages/sign_in_page.dart';
+import 'package:shamo/pages/splash_page.dart';
 import 'theme.dart';
 
 void main() {
@@ -10,23 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
-    );
-  }
-}
-
-class Homepage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Halo Semuanya',
-          style: priceTextStyle.copyWith(
-            fontSize: 30
-          ),
-        ),
-      ),
+      routes: {
+        '/':(context) => SplashPage(),
+        '/sign-in': (context) => SignInPage()
+      },
     );
   }
 }
---
+++ lib/pages/sign_in_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Ini Splash Screen'
+        ),
+      ),
+    );
+  }
+}
