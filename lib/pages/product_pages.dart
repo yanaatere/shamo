eb8001efbed9082e8cb4f892bb7aa1151f1e6bc3
yanaatere <yanaandika@gmail.com>
2022-06-18 15:55:22 +0700

    Create Carousel Slider Page And Indicator

--- lib/pages/product_pages.dart
+++ lib/pages/product_pages.dart
@@ -1,12 +1,36 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
-class ProductPage extends StatelessWidget {
-  const ProductPage({Key? key}) : super(key: key);
+class ProductPage extends StatefulWidget {
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  List images = [
+    'assets/display_shoes.png',
+    'assets/display_shoes.png',
+    'assets/display_shoes.png',
+  ];
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Widget indicator(int index) {
+      return Container(
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: currentIndex == index ? primaryColor : Color(0xffC4C4C4)),
+      );
+    }
+
     Widget header() {
+      int index = -1;
+
       return Column(
         children: [
           Container(
@@ -21,10 +45,35 @@ class ProductPage extends StatelessWidget {
                     Navigator.pop(context);
                   },
                 ),
-                Icon(Icons.shopping_bag, color: backgroundColor1,)
+                Icon(
+                  Icons.shopping_bag,
+                  color: backgroundColor1,
+                )
               ],
             ),
-          )
+          ),
+          CarouselSlider(
+              items: images
+                  .map((images) => Image.asset(
+                        images,
+                        width: MediaQuery.of(context).size.width,
+                        height: 310,
+                        fit: BoxFit.cover,
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                  initialPage: 0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  })),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: images.map((e) {
+                index++;
+                return indicator(index);
+              }).toList())
         ],
       );
     }
